03ef0112fff47c34d412895a199b7f3ea3bda7ae
Aniket Waliyan <30599754+Aniket-Wali@users.noreply.github.com>
2021-09-01 14:00:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
 import React, {Component, useState} from "react";
-import "./../styles/App.css";
+import '../styles/App.css';
 
-function App() {
-  return (
-    <div id="main">
-      // Do not alter the main div
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+		super(props);
+		this.state = {
+			pHidden : true,
+		}
+	};
+
+
+    render() {
+    	return(
+    		<div id="main">
+				<button id="click" onClick={ ()=>{
+					this.setState({pHidden : false})
+				}}>Show Para</button>
+				{this.state.pHidden ? null : (
+					<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+				)}
+				
+    		</div>
+    	);
+    }
 }
 
 
 export default App;
+
